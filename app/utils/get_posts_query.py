a26b3ca91fde1c1e76702a1923bf3ba0abe73730
safaraliyevelmir <safaraliyevelmir@gmail.com>
2024-07-14 20:17:48 +0400

    first commit

--- app/utils/get_posts_query.py
+++ app/utils/get_posts_query.py
@@ -3,6 +3,6 @@ from app.database import Post
 
 @ttl_cache(maxsize=128, ttl=300)
 def get_all_posts_query(db):
-    """Get all posts"""
+    """Get all posts from the database and cache the result"""
     posts = db.query(Post).all()
     return posts
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,4 +1,3 @@
-import uuid
 from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
@@ -8,20 +7,21 @@ from app.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def get_password_hash(password):
+def get_password_hash(password: str) -> str:
+    """Get the hashed password"""
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
+    """Verify the password"""
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict):
+    """Create an access token"""
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
-def create_activation_token() -> str:
-    return uuid.uuid4().hex
 
 
