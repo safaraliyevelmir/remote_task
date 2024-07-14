81585c8e4213c9a99b159d6d1a7d7cde42576669
safaraliyevelmir <safaraliyevelmir@gmail.com>
2024-07-14 20:27:21 +0400

    .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app.routes import auth
+from app.routes import posts
 
 app = FastAPI()
 
 app.include_router(auth.router)
-
+app.include_router(posts.router)
 
 @app.get("/")
 def read_root():
